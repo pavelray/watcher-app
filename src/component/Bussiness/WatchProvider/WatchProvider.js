7a13d4821ec78eca32b1bfe58313748386c4fbe5
Pavel Ray <pray@falabella.cl>
2022-11-07 18:36:49 +0530

    Updated watch provider

--- src/component/Bussiness/WatchProvider/WatchProvider.js
+++ src/component/Bussiness/WatchProvider/WatchProvider.js
@@ -1,13 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import axios from 'axios';
 import React, { useCallback, useEffect, useState } from 'react'
-import { MEDIA_TYPE } from '../../../utils/constants';
+import { API_IMAGE_URL, MEDIA_TYPE } from '../../../utils/constants';
 import { getAllCountriesAPIUrl, getProvidersAPIUrl } from '../../../utils/helperMethods';
+import "./WatchProvider.scss";
 
 const WatchProvider = ({ type = MEDIA_TYPE.MOVIE, id }) => {
+    const USER_COUNTRY = 'IN';
     const [countryData, setCountryData] = useState([]);
     const [providers, setProviderData] = useState({});
-    const [selectedProvider, setSelectedProvider] = useState([]);
+    // const [selectedProvider, setSelectedProvider] = useState([]);
 
     const getAllCountries = useCallback(() => {
         const fetchCountries = async () => {
@@ -29,26 +31,26 @@ const WatchProvider = ({ type = MEDIA_TYPE.MOVIE, id }) => {
                 url
             );
             const { data } = resp;
-            setProviderData(data.results);
+            setProviderData(data.results[USER_COUNTRY]);
         };
         fetchProviders();
     }, []);
 
 
     useEffect(() => {
-        getAllCountries();
+        // getAllCountries();
         getProviders();
     }, []);
 
-    const handleOnChange = (event) => {
-        const { value } = event.target;
-        const selectValue = providers[value].flatrate;
-        setSelectedProvider(selectValue);
-    }
+    // const handleOnChange = (event) => {
+    //     const { value } = event.target;
+    //     const selectValue = providers[value].flatrate;
+    //     setSelectedProvider(selectValue);
+    // }
 
     return (
-        <div>
-            <select onChange={handleOnChange}>
+        <div className='provider-container'>
+            {/* <select onChange={handleOnChange}>
                 {
                     countryData?.map((country) => (
                         <option value={country.iso_3166_1} key={country.iso_3166_1}>{country.english_name}</option>
@@ -61,7 +63,18 @@ const WatchProvider = ({ type = MEDIA_TYPE.MOVIE, id }) => {
                         <div>{provider.provider_name}</div>
                     ))
                 }
+            </div> */}
+            <div className='provider-title'>
+                Streaming
             </div>
+            {
+                providers.flatrate && providers.flatrate.map((ott) => (
+                    <div key={ott.provider_id} className='provider-logo'>
+                        <img src={`${API_IMAGE_URL}/w45/${ott.logo_path}`} alt={ott.provider_name} />
+                    </div>
+                ))
+            }
+
         </div>
     )
 }
